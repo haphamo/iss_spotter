aecccdad0d674cb1f31b9d4910bd2898ddd7aee2
Ha Pham <hapham0392@gmail.com>
2019-09-27 00:55:07 +0000

    Added function and callback for fetching coodrinates of IP address

--- iss.js
+++ iss.js
@@ -1,20 +1,37 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
-    } 
+    }
     if (response.statusCode !== 200) {
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
-    return;
-    } //there is an IP, call back 
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
+};
 
-}
+const fetchCoordsByIP = function(ip, callback) {
+  request("https://ipvigilante.com/json/invalidiphere", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const coordinates = {};
+    coordinates["latitude"] = JSON.parse(body).data.latitude;
+    coordinates["longitude"] = JSON.parse(body).data.longitude;
+    callback(null, coordinates);
+  });
+};
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
